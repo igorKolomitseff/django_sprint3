e5affecde9fe7e869ff13985686c6992b4271ba2
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2024-02-29 19:51:44 +0300

    Switch the admin panel to Russian localization

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -8,34 +8,71 @@ User = get_user_model()
 
 
 class Category(BaseModel):
-    title = models.CharField(max_length=256)
-    description = models.TextField()
-    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
+    description = models.TextField(verbose_name='Описание')
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Идентификатор',
+        help_text=(
+            'Идентификатор страницы для URL; '
+            'разрешены символы латиницы, цифры, дефис и подчёркивание.'
+        )
+    )
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.title
 
 
 class Location(BaseModel):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=256, verbose_name='Название места')
+
+    class Meta:
+        verbose_name = 'местоположение'
+        verbose_name_plural = 'Местоположения'
+
+    def __str__(self):
+        return self.name
 
 
 class Post(BaseModel):
-    title = models.CharField(max_length=256)
-    text = models.TextField()
-    pub_date = models.DateTimeField()
+    title = models.CharField(max_length=256, verbose_name='Заголовок')
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время публикации',
+        help_text=(
+            'Если установить дату и время в будущем — '
+            'можно делать отложенные публикации.'
+        )
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Автор публикации'
     )
     location = models.ForeignKey(
         Location,
         blank=True,
         null=True,
         on_delete=models.SET_NULL,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Местоположение'
     )
     category = models.ForeignKey(
         Category,
         null=True,
         on_delete=models.SET_NULL,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Категория'
     )
+
+    class Meta:
+        verbose_name = 'публикация'
+        verbose_name_plural = 'Публикации'
+
+    def __str__(self):
+        return self.title
