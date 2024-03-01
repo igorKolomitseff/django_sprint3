7e8a6e0495102a9be69ca0c6f0302c14d4ca9f60
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2024-03-01 15:18:37 +0300

    Add Django Debug Toolbar to the project

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,6 +1,7 @@
 """blogicum URL Configuration."""
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
 
 
 urlpatterns = [
@@ -8,3 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls', namespace='blog')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
