a10790c51be86612c814d0676e0702477ae871bf
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2024-03-03 19:59:42 +0300

    Fix linter's errors

--- blogicum/core/models.py
+++ blogicum/core/models.py
@@ -7,7 +7,10 @@ class BaseModel(models.Model):
         verbose_name='Опубликовано',
         help_text='Снимите галочку, чтобы скрыть публикацию.'
     )
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Добавлено')
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Добавлено'
+    )
 
     class Meta:
         abstract = True
