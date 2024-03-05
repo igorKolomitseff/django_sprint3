4f25e00d24b6f02ad8b673638bff7f2baa78ff14
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2024-03-05 21:32:07 +0300

    Update models in the blog

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,16 +1,46 @@
-from django.db import models
-from django.contrib.auth import get_user_model
+from datetime import datetime
 
-from core.models import BaseModel
+from django.contrib.auth import get_user_model
+from django.db import models
 
 
 User = get_user_model()
+
+
+class BaseModel(models.Model):
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано',
+        help_text='Снимите галочку, чтобы скрыть публикацию.'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Добавлено'
+    )
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        field_values = []
+        for field in self._meta.get_fields():
+            if hasattr(field, 'verbose_name'):
+                value = getattr(self, field.name, '')
+                if isinstance(value, str):
+                    value = f'{value:.50}'
+                elif isinstance(value, datetime):
+                    value = value.strftime('%m-%d-%Y, %H:%M:%S')
+                field_values.append(
+                    f'{field.verbose_name}: {value}'
+                )
+        return ' | '.join(field_values)
 
 
 class Category(BaseModel):
     title = models.CharField(max_length=256, verbose_name='Заголовок')
     description = models.TextField(verbose_name='Описание')
     slug = models.SlugField(
+        max_length=64,
         unique=True,
         verbose_name='Идентификатор',
         help_text=(
@@ -23,9 +53,6 @@ class Category(BaseModel):
         verbose_name = 'категория'
         verbose_name_plural = 'Категории'
 
-    def __str__(self):
-        return self.title
-
 
 class Location(BaseModel):
     name = models.CharField(max_length=256, verbose_name='Название места')
@@ -33,9 +60,6 @@ class Location(BaseModel):
     class Meta:
         verbose_name = 'местоположение'
         verbose_name_plural = 'Местоположения'
-
-    def __str__(self):
-        return self.name
 
 
 class Post(BaseModel):
@@ -51,7 +75,6 @@ class Post(BaseModel):
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts',
         verbose_name='Автор публикации'
     )
     location = models.ForeignKey(
@@ -59,21 +82,17 @@ class Post(BaseModel):
         blank=True,
         null=True,
         on_delete=models.SET_NULL,
-        related_name='posts',
         verbose_name='Местоположение'
     )
     category = models.ForeignKey(
         Category,
         null=True,
         on_delete=models.SET_NULL,
-        related_name='posts',
         verbose_name='Категория'
     )
 
     class Meta:
         verbose_name = 'публикация'
         verbose_name_plural = 'Публикации'
+        default_related_name = 'posts'
         ordering = ('-pub_date',)
-
-    def __str__(self):
-        return self.title
